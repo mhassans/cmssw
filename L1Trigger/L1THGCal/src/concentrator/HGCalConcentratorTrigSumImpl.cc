404171161fac06426a5b51b5ca9625b372fab368
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2020-12-10 09:51:58 +0100

    quickfix trigger sum detid

--- L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorTrigSumImpl.cc
+++ L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorTrigSumImpl.cc
@@ -1,4 +1,6 @@
 #include "L1Trigger/L1THGCal/interface/concentrator/HGCalConcentratorTrigSumImpl.h"
+#include "DataFormats/ForwardDetId/interface/HGCalTriggerDetId.h"
+#include "DataFormats/ForwardDetId/interface/HGCSiliconDetIdToModule.h"
 
 HGCalConcentratorTrigSumImpl::HGCalConcentratorTrigSumImpl(const edm::ParameterSet& conf) {}
 
@@ -17,12 +19,19 @@ void HGCalConcentratorTrigSumImpl::doSum(uint32_t module_id,
     hwptsum += trigCell.hwPt();
   }
   if (!trigCellVecInput.empty()) {
+    uint32_t id0 = trigCellVecInput[0].detId();
+    uint32_t tsid = module_id;
+    if(triggerTools_.isSilicon(id0)) {
+      HGCalTriggerDetId id0si(id0);
+      DetId::Detector det = (id0si.subdet() == HGCalTriggerSubdetector::HGCalEETrigger) ? DetId::HGCalEE : DetId::HGCalHSi;
+      tsid = HGCSiliconDetId(det, id0si.zside(), id0si.type(), id0si.layer(), id0si.waferU(), id0si.waferV(), 0, 0);
+    }
     GlobalPoint module_pos = triggerTools_.getTriggerGeometry()->getModulePosition(module_id);
 
     math::PtEtaPhiMLorentzVector p4(ptsum, module_pos.eta(), module_pos.phi(), 0);
     l1t::HGCalTriggerSums ts;
     ts.setP4(p4);
-    ts.setDetId(module_id);
+    ts.setDetId(tsid);
     ts.setPosition(module_pos);
     ts.setMipPt(mipptsum);
     ts.setHwPt(hwptsum);
