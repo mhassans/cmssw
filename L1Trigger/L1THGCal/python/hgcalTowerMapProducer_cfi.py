68a235ae516b4b6546ce11b5ef17f42168ea4e6b
MohammadHassan <mhhassanshahi@yahoo.com>
2021-05-28 16:43:38 +0100

    test

--- L1Trigger/L1THGCal/python/hgcalTowerMapProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalTowerMapProducer_cfi.py
@@ -13,10 +13,19 @@ L1TTriggerTowerConfig_etaphi = cms.PSet(readMappingFile=cms.bool(False),
                                         binsEta=cms.vdouble(),
                                         binsPhi=cms.vdouble())
 
-L1TTriggerTowerConfig_energySplit = L1TTriggerTowerConfig_etaphi.clone(
-    var1=cms.double(155.2213),
-    var2=cms.double(5435.221),
-)
+
+L1TTriggerTowerConfig_energySplit = cms.PSet(readMappingFile=cms.bool(False),
+                                        doNose=cms.bool(False),
+                                        var1=cms.double(155.2213),
+                                        var2=cms.double(5435.221),
+                                        minEta=cms.double(1.479),
+                                        maxEta=cms.double(3.0),
+                                        minPhi=cms.double(-1*math.pi),
+                                        maxPhi=cms.double(math.pi),
+                                        nBinsEta=cms.int32(18),
+                                        nBinsPhi=cms.int32(72),
+                                        binsEta=cms.vdouble(),
+                                        binsPhi=cms.vdouble())
 
 towerMap2D_parValues = cms.PSet( useLayerWeights = cms.bool(False),
                                  layerWeights = cms.vdouble(),
